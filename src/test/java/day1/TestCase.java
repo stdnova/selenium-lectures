bd7b291063de235f900b3d62119fb7a9af8cff60
Kenan Mammadli <kenan@doe.org>
2024-06-08 16:11:59 -0400

    initial commit

--- src/test/java/day1/TestCase.java
+++ src/test/java/day1/TestCase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class TestCase {
 
@@ -14,31 +15,42 @@ public class TestCase {
 // Pre-requisite: Chrome Browser should be open and maximized
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+  try {
+
+      driver.manage().window().maximize();
 
 // 1. Go to google.com
-        driver.get("https://www.google.com/");
+      driver.get("https://www.google.com/");
 
 // 2. Type a search term and search (e.g dragon)
-        //locate the search bar
-       WebElement searchBar = driver.findElement(By.name("q"));
-       //type the term and hit enter
-        String expectedTerm = "Dragon";
-        searchBar.sendKeys(expectedTerm, Keys.ENTER);
+      //locate the search bar
+      WebElement searchBar = driver.findElement(By.name("q"));
+      //type the term and hit enter
+      String expectedTerm = "Dragon";
+      searchBar.sendKeys(expectedTerm, Keys.ENTER);
 // 3. On the search results page, verify the title of the page has the same term
 
-        //chained version
-        // driver.findElement(By.name("q")).sendKeys(expectedTerm, Keys.ENTER);
+      //chained version
+      // driver.findElement(By.name("q")).sendKeys(expectedTerm, Keys.ENTER);
 
-        String actualTitle = driver.getTitle();
+      String expectedTitle = expectedTerm + " -Google Search";
+      String actualTitle = driver.getTitle();
 
-        if (actualTitle.contains(expectedTerm)) {
-            System.out.printf("PASS");
-        } else {
-            throw new AssertionError("Test failed. The actual title was: " + actualTitle + " The expected term: " + expectedTerm);
-        }
+//        if (actualTitle.contains(expectedTerm)) {
+//            System.out.printf("PASS");
+//        } else {
+//            throw new AssertionError("Test failed. The actual title was: " + actualTitle + " The expected term: " + expectedTerm);
+//        }
 
-driver.quit();
+      Assert.assertEquals(actualTitle, expectedTitle);
+      Assert.assertTrue(actualTitle.contains(expectedTerm));
+
+  } finally {
+
+      driver.quit();
+
+  }
 
     }
 }
