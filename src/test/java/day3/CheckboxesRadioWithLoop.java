fc4491192d149bf979b0e95e955648c9d70daedb
Kenan Mammadli <kenan@doe.org>
2024-06-12 19:41:37 -0400

    initial commit

--- src/test/java/day3/CheckboxesRadioWithLoop.java
+++ src/test/java/day3/CheckboxesRadioWithLoop.java
@@ -9,8 +9,9 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
-public class HandlingCheckboxesLoop {
+public class CheckboxesRadioWithLoop {
 
     @Test
     public void testCase() throws InterruptedException {
@@ -26,15 +27,25 @@ public class HandlingCheckboxesLoop {
 
         List<WebElement> elements = driver.findElements(By.cssSelector("input[type='checkbox']"));
 
+
         for (WebElement element : elements) {
             if (!element.isSelected()){
                 element.click();
             }
-
         }
 
         driver.findElement(By.cssSelector(".btnDeleteSelected")).click();
 
+        driver.findElement(By.cssSelector("a[href='Process.aspx']")).click();
+
+        driver.findElement(By.cssSelector("input[value='MasterCard']")).click();
+
+        List<WebElement> radioList = driver.findElements(By.cssSelector("input[type='radio']"));
+
+        for (WebElement button : radioList) {
+            Random rand = new Random();
+            radioList.get(rand.nextInt(radioList.size())).click();
+        }
 
         Thread.sleep(3000);
 driver.quit();
